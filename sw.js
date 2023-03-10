2063e4ccc025d76cb2ed24a580e1df04557235ee
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2023-03-10 00:56:17 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -35,3 +35,12 @@ self.addEventListener('fetch', event => {
         }
     })());
 });
+
+// a
+$(document).keydown(function (event) {
+    if (event.keyCode == 123) { // Prevent F12
+        return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+        return false;
+    }
+});
